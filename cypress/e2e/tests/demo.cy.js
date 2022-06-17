fe106b3f42127c4242d62c2b9d4d0e3287cf4950
Naresh9162 <89434905+Naresh9162@users.noreply.github.com>
2022-06-17 11:47:03 +0530

    Update demo.cy.js

--- cypress/e2e/tests/demo.cy.js
+++ cypress/e2e/tests/demo.cy.js
@@ -1,7 +1,7 @@
 describe('Resetting Password', function(){
     it('Enabling forget password using security Question',function(){
        // cy.visit("/sspr")
-        cy.exec(echo nareshsharma)
+        cy.exec(echo 'nareshsharma')
        // cy.exec('bash cypress/fixtures/demotest.sh') 
       // cy.copyXmlFile()
     })
